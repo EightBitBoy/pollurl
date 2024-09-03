874d6fe3b23118fd4178392aba886b20ec2e8c4c
Stefan Härtel <haertel.dev@gmail.com>
2024-09-03 21:22:55 +0200

    Playing around with rust

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+use std::io::{stdout, Write};
+use curl::easy::Easy;
+
 fn main() {
-  playground();
+  // playground();
+
+  let mut curl = Easy::new();
+
+  curl.url("https://www.sfffdsdafr3wfd.org/").unwrap();
+  curl.perform().unwrap();
+
+  let response_code = -1;
+  response_code = curl.response_code().unwrap();
+
+  println!("{}", response_code);
 }
 
 fn playground(){
