358c9f44de4a723d94b1248ed515ff7317361a63
Stefan Härtel <haertel.dev@gmail.com>
2024-09-08 00:53:46 +0200

    Prevent "unused" warning for playground function

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
-use std::io::{stdout, Write};
-use curl::easy::Easy;
+// use std::io::{stdout, Write};
+// use curl::easy::Easy;
 
 fn main() {
   // playground();
 
-  let mut curl = Easy::new();
+  // let mut curl = Easy::new();
 
-  curl.url("https://www.google.de/").unwrap();
-  curl.perform().unwrap();
+  // curl.url("https://www.google.de/").unwrap();
+  // curl.perform().unwrap();
 
-  let mut response_code: u32 = 0;
-  response_code = curl.response_code().unwrap();
+  // let mut response_code: u32 = 0;
+  // response_code = curl.response_code().unwrap();
 
-  println!("{}", response_code);
+  // println!("{}", response_code);
+
+
 }
 
+#[allow(dead_code)]
 fn playground(){
   println!("Hello, world!");
 
