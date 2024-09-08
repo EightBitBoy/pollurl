cacd6a73ab1db9b2997d49c4fbf1858da7be4a9e
Stefan Härtel <haertel.dev@gmail.com>
2024-09-08 22:20:23 +0200

    Use verbose test output

--- integration_tests.py
+++ integration_tests.py
@@ -1,5 +1,12 @@
+from cli_test_helpers import shell
+
 def func(x):
-    return x + 1
+  return x + 1
 
 def test_answer():
-    assert func(4) == 5
+  assert func(4) == 5
+
+def test_example():
+  result = shell("echo 'hello'")
+  assert result.exit_code == 0
+  assert "hell" in result.stdout
