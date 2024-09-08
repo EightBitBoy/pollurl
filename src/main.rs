ec4fde4a42e775cbf51041ef6d46827399b7da62
Stefan Härtel <haertel.dev@gmail.com>
2024-09-08 14:01:53 +0200

    Implement timeout

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::time;
 use std::thread;
+use std::process;
 
 use clap::Parser;
+
+//TODO Create an enum of exit codes
+//https://blog.rust-lang.org/2022/05/19/Rust-1.61.0.html
 
 
 /// waiturl: Wait for a specific HTTP response by continuously polling a URL
@@ -17,40 +21,41 @@ struct Arguments {
 
   /// Timeout in seconds until the program terminates, runs infinitely otherwise
   #[arg(short, long, default_value_t = 0)]
-  timeout: u8,
+  timeout: u64,
 
   /// The interval in seconds between each poll request. This does not include the time for the actual request
   #[arg(short, long, default_value_t = 1)]
   interval: u64
+
+  //TODO -q arg to suppress output during polling
 }
 
 fn main() {
   let arguments = Arguments::parse();
   let interval_duration_seconds = time::Duration::from_secs(arguments.interval);
+  let start_time = time::Instant::now();
 
   loop {
-    poll();
+    poll(&arguments.url, arguments.response_code);
+
+    if arguments.timeout > 0 && start_time.elapsed().as_secs() >= arguments.timeout {
+      println!("Polling timeout after {} seconds", arguments.timeout);
+      process::exit(1);
+    }
+
     thread::sleep(interval_duration_seconds);
   };
 
   // playground();
 
   // let mut curl = Easy::new();
-
   // curl.url("https://www.google.de/").unwrap();
   // curl.perform().unwrap();
-
-  // let mut response_code: u32 = 0;
   // response_code = curl.response_code().unwrap();
-
-  // println!("{}", response_code);
-
-  // for _ in 0..arguments.count {
-  // }
 }
 
-fn poll() {
-  println!("Polling!");
+fn poll(url: &String, response_code: u8) {
+  println!("{}: 404, expected {}", url, response_code);
 }
 
 #[allow(dead_code)]
@@ -61,4 +66,7 @@ fn playground(){
   println!("{}", message);
 
   dbg!(message);
+
+  for _ in 0..2 {
+  }
 }
